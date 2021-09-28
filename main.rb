fc3ca3719b130109714ff04ff468167ca7f09b96
Usman Suleiman <usmansbk@gmail.com>
2021-09-28 01:50:23 +0100

    Fix linter error

--- main.rb
+++ main.rb
@@ -1,30 +1,30 @@
 @international_morse_code = {
-    ".-" => "A",
-    "-..." => "B",
-    "-.-." => "C",
-    "-.." => "D",
-    "." => "E",
-    "..-." => "F",
-    "--." => "G",
-    "...." => "H",
-    ".." => "I",
-    ".---" => "J",
-    "-.-" => "K",
-    ".-.." => "L",
-    "--" => "M",
-    "-." => "N",
-    "---" => "O",
-    ".--." => "P",
-    "--.-" => "Q",
-    ".-." => "R",
-    "..." => "S",
-    "-" => "T",
-    "..-" => "U",
-    "...-" => "V",
-    ".--" => "W",
-    "-..-" => "X",
-    "-.--" => "Y",
-    "--.." => "Z"
+    '.-' => 'A',
+    '-...' => 'B',
+    '-.-.' => 'C',
+    '-..' => 'D',
+    '.' => 'E',
+    '..-.' => 'F',
+    '--.' => 'G',
+    '....' => 'H',
+    '..' => 'I',
+    '.---' => 'J',
+    '-.-' => 'K',
+    '.-..' => 'L',
+    '--' => 'M',
+    '-.' => 'N',
+    '---' => 'O',
+    '.--.' => 'P',
+    '--.-' => 'Q',
+    '.-.' => 'R',
+    '...' => 'S',
+    '-' => 'T',
+    '..-' => 'U',
+    '...-' => 'V',
+    '.--' => 'W',
+    '-..-' => 'X',
+    '-.--' => 'Y',
+    '--..' => 'Z'
 }
 
 def decode_char(letter_code)
@@ -33,12 +33,12 @@ def decode_char(letter_code)
 end
 
 def decode_word(word_code)
-    letters = word_code.split(" ").map { |letter_code| decode_char(letter_code) }
+    letters = word_code.split(' ').map { |letter_code| decode_char(letter_code) }
     letters.join('') 
 end
 
 def decode(morse_code)
-    words = morse_code.split("   ").map { |word_code| decode_word(word_code)}
+    words = morse_code.split('   ').map { |word_code| decode_word(word_code)}
     words.join(' ')
 end
 
